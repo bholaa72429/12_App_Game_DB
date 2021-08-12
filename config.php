386ac8eadf386f5fce01b011f9ac55cd0fbfde71
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-08-13 11:54:32 +1200

    Config created
    created config file and added it to index

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set('display_error','On'); // change to 'Off'when live
 
-define ("DB_NAME","bholaa72429");
+define ("DB_NAME","bholaa72429_L2_Apps");
 define ("DB_USERNAME","_bholaa72429");
 define ("DB_PASSWORD","f75b4bc51877b7704997c3fc28a4d7ff");
 define ("DB_HOST","localhost");
